bd184f93f43f344086f05fee1a7dd1f5d5d1d99b
fadliselaz <fadliselaz@gmail.com>
2018-08-06 00:00:44 +0700

    update pakong

---
+++ basic/integer.py
@@ -0,0 +1,27 @@
+# pembulatan
+# pembulatan bisa digunakan untuk nilai float
+
+# n = 3.75
+# print(round(n))
+#
+# print("\n")
+#
+# # round juga bisa ditambahkan mau berapa dibulatkannya
+# print(round(n, 1))
+# print(round(n))
+
+# >>> casting >>>
+# casting adalah proses perubahan type data
+
+num1 = "100"
+num2 = "200"
+
+# jika di jumlahkan
+print(num1 + num2)
+#maka hasilnya akan 100200
+#karena kedua type data tersebut merupakan string
+
+#maka harus di rubah dahulu..
+num1 = int(num1)
+num2 = int(num2)
+print(num1 + num2)
---
+++ basic/string.py
@@ -0,0 +1,45 @@
+# nama = "SELASTIO FADLI"
+# print(nama.lower())
+
+# >>>>>>>>>>> contoh untuk mencari item dalam string >>>>>>>>>>
+# text = "SELAMAT JALAN KEKASIH"
+# text = text.lower()
+# cari = text.find("jalan")
+#
+# if cari > 0:
+#     print("ada..")
+# else:
+#     print("ngak ada..")
+
+# # >>>> fromating text >>>>>>
+# # untuk di ingat ini berlaku hanya di python 3 keatas..
+# nama = "selastio fadli rahman"
+# event = "Acara pelepasan Alumni MTID"
+# result = f"selamat datang {nama}, di acara {event}"
+# print(result)
+#
+# # untuk python 2 silakan gunakan ini :
+# nama2 = "Budi Wibowo"
+# event2 = "Peresmian MTID"
+# result2 = "selamat datang {} di acara {}".format(nama2,event2)
+# print(result)
+#
+
+# >>>> help / bantuan didalam script python >>>>>>
+# ada dua type :
+# dir(berisiVariable)
+# help(berisiSyntax atau func)
+
+# print(help(str))
+
+# nama = "fadliselaz"
+# print(dir(nama))
+
+
+# penambahan string >>>>>>>>>>>
+
+nama1 = "selastio"
+nama1 += "fadli"
+print(nama1)
+
+print(nama1)
